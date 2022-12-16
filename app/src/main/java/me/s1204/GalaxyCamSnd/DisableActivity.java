732198385c0cd15ff140f5470191da632572b496
Syuugo <pub@s1204.me>
2022-12-16 23:50:31 +0900

    Initial Commit

---
+++ app/src/main/java/me/s1204/GalaxyCamSnd/DisableActivity.java
@@ -0,0 +1,21 @@
+package me.s1204.GalaxyCamSnd;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import static android.provider.Settings.System.putInt;
+
+public class DisableActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    putInt(getContentResolver(), "csc_pref_camera_forced_shuttersound_key", 1);
+    super.onDestroy();
+    finishAndRemoveTask();
+    Intent camera=new Intent();
+    camera.setClassName("com.sec.android.app.camera", "com.sec.android.app.camera.Camera");
+    startActivity(camera);
+  }
+}
