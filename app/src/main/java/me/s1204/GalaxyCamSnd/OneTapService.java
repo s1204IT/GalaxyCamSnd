1174c56caa50375dfa698bac0abb997381ef8d9b
Syuugo <pub@s1204.me>
2023-01-04 06:05:30 +0900

    minSdkを変更

--- app/src/main/java/me/s1204/GalaxyCamSnd/OneTapService.java
+++ app/src/main/java/me/s1204/GalaxyCamSnd/OneTapService.java
@@ -1,13 +1,10 @@
 package me.s1204.GalaxyCamSnd;
 
-import android.annotation.TargetApi;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
-import static android.os.Build.VERSION_CODES.N;
 import static android.provider.Settings.System.putInt;
 
-@TargetApi(N)
 public class OneTapService extends TileService {
     public void onClick() {
         Tile tile = getQsTile();
