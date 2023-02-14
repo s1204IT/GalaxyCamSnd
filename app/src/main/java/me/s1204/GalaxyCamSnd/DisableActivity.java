b839848713e361e1370d6911783b36a5477c3be2
Syuugo <pub@s1204.me>
2023-02-15 01:08:01 +0900

    とりあえず動く様に修正

--- app/src/main/java/me/s1204/GalaxyCamSnd/DisableActivity.java
+++ app/src/main/java/me/s1204/GalaxyCamSnd/DisableActivity.java
@@ -11,13 +11,17 @@ import static android.provider.Settings.System.putInt;
 public class DisableActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    if (!canWrite(this)) {
-      startActivity(new Intent("android.settings.action.MANAGE_WRITE_SETTINGS", parse("package:me.s1204.GalaxyCamSnd")).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    if (!canWrite(getApplicationContext())) {
       finishAndRemoveTask();
+      startActivity(new Intent("android.settings.action.MANAGE_WRITE_SETTINGS", parse("package:me.s1204.GalaxyCamSnd")).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    } else {
+      super.onCreate(savedInstanceState);
+      finishAndRemoveTask();
+      try {
+        putInt(getContentResolver(), "csc_pref_camera_forced_shuttersound_key", 0);
+      } finally {
+        startActivity((new Intent()).setClassName("com.sec.android.app.camera", "com.sec.android.app.camera.Camera"));
+      }
     }
-    super.onCreate(savedInstanceState);
-    putInt(getContentResolver(), "csc_pref_camera_forced_shuttersound_key", 0);
-    finishAndRemoveTask();
-    startActivity((new Intent()).setClassName("com.sec.android.app.camera", "com.sec.android.app.camera.Camera"));
   }
 }
