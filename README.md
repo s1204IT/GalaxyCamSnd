48ccd1fda766bf3cc6dacd624d43a9c2eacc1f2b
Syuugo <pub@s1204.me>
2023-06-30 22:46:12 +0900

    v1.2

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Galaxy CamSnd
 
-## TODO
-- targetSdk を 33 (23以降) にする
-- アクティビティから起動する際に､ タイルサービスと同期する
-- `BootCompletedReceiver` を実装する
+純正カメラアプリのシャッター音をオフにします
---
+++ app/src/main/java/me/s1204/galaxycamsnd/DisableActivity.java
@@ -0,0 +1,20 @@
+package me.s1204.galaxycamsnd;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.net.Uri;
+import android.provider.Settings;
+
+public class DisableActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finishAndRemoveTask();
+        if (Settings.System.canWrite(this)) {
+            Settings.System.putInt(getContentResolver(), "csc_pref_camera_forced_shuttersound_key", 0);
+            startActivity((new Intent()).setClassName("com.sec.android.app.camera", "com.sec.android.app.camera.Camera"));
+        } else {
+            startActivity(new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName())).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+    }
+}
