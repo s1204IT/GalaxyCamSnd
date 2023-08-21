8092b607ff1743cd175313398ab70223207b7cea
Syuugo Mizoguchi <pub@s1204.me>
2023-08-21 19:45:04 +0900

    CameraActivity を最適化
    Signed-off-by: Syuugo Mizoguchi <pub@s1204.me>

--- app/src/main/java/me/s1204/galaxycamsnd/CameraActivity.java
+++ app/src/main/java/me/s1204/galaxycamsnd/CameraActivity.java
@@ -10,13 +10,15 @@ import android.widget.Toast;
 public class CameraActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent;
         finishAndRemoveTask();
         if (Settings.System.canWrite(this)) {
             Settings.System.putInt(getContentResolver(), "csc_pref_camera_forced_shuttersound_key", 0);
-            startActivity((new Intent()).setClassName("com.sec.android.app.camera", "com.sec.android.app.camera.Camera"));
+            intent = new Intent().setClassName("com.sec.android.app.camera", "com.sec.android.app.camera.Camera");
         } else {
             Toast.makeText(this, R.string.write, Toast.LENGTH_LONG).show();
-            startActivity(new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName())).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName())).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         }
+        startActivity(intent);
     }
 }
