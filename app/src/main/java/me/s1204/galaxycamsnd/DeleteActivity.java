e079bbb4f0f2c47b2857526beb1f6a2d0931789d
Syuugo Mizoguchi <pub@s1204.me>
2023-08-21 10:01:41 +0900

    "システム設定の変更"を強制
    Signed-off-by: Syuugo Mizoguchi <pub@s1204.me>

--- app/src/main/java/me/s1204/galaxycamsnd/DeleteActivity.java
+++ app/src/main/java/me/s1204/galaxycamsnd/DeleteActivity.java
@@ -10,10 +10,11 @@ public class DeleteActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         finishAndRemoveTask();
-        try {
+        if (Settings.System.canWrite(this)) {
             Settings.System.putInt(getContentResolver(), "csc_pref_camera_forced_shuttersound_key", 1);
-        } finally {
             startActivity(new Intent(Intent.ACTION_DELETE).setData(Uri.parse("package:" + getPackageName())));
+        } else {
+            startActivity(new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName())).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
         }
     }
 }
