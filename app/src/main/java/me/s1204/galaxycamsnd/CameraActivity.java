3b01f8f9c8b366802d93d1ffb9639b5edeb97065
Syuugo <pub@s1204.me>
2023-08-21 10:22:44 +0900

    v1.3.2

--- app/src/main/java/me/s1204/galaxycamsnd/CameraActivity.java
+++ app/src/main/java/me/s1204/galaxycamsnd/CameraActivity.java
@@ -5,15 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.net.Uri;
 import android.provider.Settings;
+import android.widget.Toast;
 
-public class DeleteActivity extends Activity {
+public class CameraActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         finishAndRemoveTask();
         if (Settings.System.canWrite(this)) {
-            Settings.System.putInt(getContentResolver(), "csc_pref_camera_forced_shuttersound_key", 1);
-            startActivity(new Intent(Intent.ACTION_DELETE).setData(Uri.parse("package:" + getPackageName())));
+            Settings.System.putInt(getContentResolver(), "csc_pref_camera_forced_shuttersound_key", 0);
+            startActivity((new Intent()).setClassName("com.sec.android.app.camera", "com.sec.android.app.camera.Camera"));
         } else {
+            Toast.makeText(this, "@string/write", Toast.LENGTH_LONG).show();
             startActivity(new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName())).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
         }
     }
--- app/src/main/java/me/s1204/galaxycamsnd/DisableActivity.java
+++ app/src/main/java/me/s1204/galaxycamsnd/DisableActivity.java
@@ -5,15 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.net.Uri;
 import android.provider.Settings;
+import android.widget.Toast;
 
 public class DisableActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         finishAndRemoveTask();
         if (Settings.System.canWrite(this)) {
-            Settings.System.putInt(getContentResolver(), "csc_pref_camera_forced_shuttersound_key", 0);
-            startActivity((new Intent()).setClassName("com.sec.android.app.camera", "com.sec.android.app.camera.Camera"));
+            Settings.System.putInt(getContentResolver(), "csc_pref_camera_forced_shuttersound_key", 1);
+            Toast.makeText(this, "@string/enabled", Toast.LENGTH_LONG).show();
         } else {
+            Toast.makeText(this, "@string/write", Toast.LENGTH_LONG).show();
             startActivity(new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName())).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
         }
     }
