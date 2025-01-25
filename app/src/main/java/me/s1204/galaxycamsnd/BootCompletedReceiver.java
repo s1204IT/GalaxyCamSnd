f291165eda70301ff834b2d6ee23675a85cb91e4
Syuugo <pub@s1204.me>
2025-01-25 22:00:15 +0900

    v1.5.0

--- app/src/main/java/me/s1204/galaxycamsnd/BootCompletedReceiver.java
+++ app/src/main/java/me/s1204/galaxycamsnd/BootCompletedReceiver.java
@@ -6,9 +6,9 @@ import android.content.Intent;
 import android.provider.Settings;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
-    public void onReceive(Context c, Intent i) {
-        if (Settings.System.canWrite(c)) {
-            Settings.System.putInt(c.getContentResolver(), CameraActivity.camera_forced_shuttersound_key, 0);
+    public void onReceive(Context context, Intent intent) {
+        if (Settings.System.canWrite(context)) {
+            Settings.System.putInt(context.getContentResolver(), CameraActivity.camera_forced_shuttersound_key, 0);
         }
     }
 }
--- app/src/main/java/me/s1204/galaxycamsnd/DeviceAdminReceiver.java
+++ app/src/main/java/me/s1204/galaxycamsnd/DeviceAdminReceiver.java
@@ -1,4 +1,43 @@
 package me.s1204.galaxycamsnd;
 
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.widget.Toast;
+
 public class DeviceAdminReceiver extends android.app.admin.DeviceAdminReceiver {
+
+    void doDisable(Context context) {
+        if (Settings.System.canWrite(context)) {
+            Settings.System.putInt(context.getContentResolver(), CameraActivity.camera_forced_shuttersound_key, 0);
+        }
+    }
+
+    void showToast(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        doDisable(context);
+    }
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        doDisable(context);
+        showToast(context, context.getString(R.string.admin_receiver_status_enabled));
+    }
+
+    @Override
+    public CharSequence onDisableRequested(Context context, Intent intent) {
+        context.startActivity(new Intent(context, DisableActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        return context.getString(R.string.admin_receiver_status_disable_warning);
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        context.startActivity(new Intent(context, DisableActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        showToast(context, context.getString(R.string.admin_receiver_status_disabled));
+    }
+
 }
